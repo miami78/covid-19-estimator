95035570f90640f582ebf9ab6acc910815f96a8e
Miami78 <miamilarry78@gmail.com>
2020-04-07 17:33:50 +0300

    add estimateCurrentlyInfected and estimateProjectedInfections

--- src/estimator.js
+++ src/estimator.js
@@ -1,10 +1,22 @@
+const reportedCases = 674;
+
+// Challenge 1
+const estimateCurrentlyInfected = reportedCases * 10;
+
+const estimateProjectedInfections = reportedCases * 50;
+
+
 const covid19ImpactEstimator = (data) => {
-  const inputData = data;
-  return {
-    data: inputData,
+  const estimate = {
+    estimateCurrentlyInfected,
+    estimateProjectedInfections
+  };
+
+  return estimate({
+    data,
     impact: {},
     severeImpact: {}
-  };
+  });
 };
 
 export default covid19ImpactEstimator;
