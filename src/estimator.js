f5c13f0fd703b38bbc4ad27e53059aa5fe088fc7
Miami78 <miamilarry78@gmail.com>
2020-04-07 17:42:43 +0300

    add infectionsByRequestedTime

--- src/estimator.js
+++ src/estimator.js
@@ -5,11 +5,17 @@ const estimateCurrentlyInfected = reportedCases * 10;
 
 const estimateProjectedInfections = reportedCases * 50;
 
+const currentlyInfected30 = estimateCurrentlyInfected * 1024;
+
+const projectedInfections30 = estimateProjectedInfections * 1024;
+
+const infectionsByRequestedTime = projectedInfections30 + currentlyInfected30;
 
 const covid19ImpactEstimator = (data) => {
   const estimate = {
     estimateCurrentlyInfected,
-    estimateProjectedInfections
+    estimateProjectedInfections,
+    infectionsByRequestedTime
   };
 
   return estimate({
