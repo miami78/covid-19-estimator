b0da277da2bdb0fa24a1cf04cd72032fd2c922ef
Miami78 <miamilarry78@gmail.com>
2020-04-09 06:06:06 +0300

    im tired

--- src/api/routes/estimator.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-const estimatorController = require('../controllers/estimator');
-
-router.post('/:responseType?', estimatorController.estimates);
-router.get('/logs', estimatorController.logs);
-
-module.exports = router;
