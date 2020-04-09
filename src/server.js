32f9f93021bee7692e0412dd7cb4a59cc00abe0d
Miami78 <miamilarry78@gmail.com>
2020-04-09 14:28:36 +0300

    test

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 import app from './app';
 
+require('dotenv').config({ path: `${__dirname}/.env` });
+
 const port = process.env.PORT || 8000;
 
-app.listen(port);
+if (process.env.NODE_ENV === 'production') {
+  app.listen(port);
+}
